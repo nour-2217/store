bddc0f88b98e552048fb7c935a4c82858f2e87dc
nour-2217 <nourhan2218594@gmail.com>
2022-06-17 14:40:13 +0200

    Tools Component Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ToolsComponent } from './tools/tools.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'tools',
+    component: ToolsComponent
+  },
+  {
+    path: '',
+    redirectTo: 'tools',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
